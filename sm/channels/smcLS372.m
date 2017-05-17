65a9ab766643341e28ec74f4d99b58b9ad60ec0a
Sergio de la Barrera <sergio@phys.cmu.edu>
2017-05-17 19:40:34 -0400

    added useful channels to sminst_LS372.mat, but did not add set/get functionality to new channels in smcLS372 driver yet

--- sm/channels/smcLS372.m
+++ sm/channels/smcLS372.m
@@ -1,17 +1,26 @@
 function val = smcLS372(ico, val, rate)
 % driver for LakeShore 372 AC Resistance Bridge and Temperature Controller
 % written by Sergio de la Barrera on 1/9/2017
+% updated with additional control channels on 5/10/2017
 
 global smdata;
 
 switch ico(2) % channel
-    case {1, 2, 3, 4, 5, 6, 7, 8}
+    case num2cell(1:16) % get temperature
         switch ico(3)
 			case 0 % get: returns temperature in Kelvin and ignores error (part after second +/- sign)
-%                 query(smdata.inst(ico(1)).data.inst, sprintf('RDGST? %d', ico(2)), '%s\n', '%s') % retrieves status of given channel; 000 means valid
                 val = query(smdata.inst(ico(1)).data.inst, sprintf('RDGK? %d', ico(2)), '%s\n', '%f');
 
 			otherwise
+				error('Operation not supported');
+        end
+    case 17 % set/get output mode
+        switch ico(3)
+			case 0 % get: output mode of sample heater (channel 0)
+                val = query(smdata.inst(ico(1)).data.inst, 'OUTMODE? 0', '%s\n', '%s');
+
+            otherwise
+%                 fprintf(smdata.inst(ico(1)).data.inst, 'OUTMODE 0,', val);
 				error('Operation not supported');
         end
 
