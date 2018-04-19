0bc63aa6ac08cac2bc1e2b7064d8c546107e44c8
Sergio de la Barrera <sergio@phys.cmu.edu>
2018-04-19 16:59:30 -0400

    updates to smc and sminst files for SeekatUNO 8-channel DC voltage source to allow smget() and ramp functionality; not a true READ, but rather a lookup of last SET value

--- sm/channels/smcSeekatUNO.m
+++ sm/channels/smcSeekatUNO.m
@@ -1,6 +1,11 @@
 function [val, rate] = smcSeekatUNO(ico, voltage, rate)
 % driver for Seekat (Arduino UNO)
-% hacked together on 2017-06-29 by Elena Lazareva:
+% hacked together on 2017-06-29 by Elena Lazareva
+% modified on 2018-04-19 by Sergio de la Barrera
+%   - added pseudo-SMGET functionality (REPORTS LAST SET VALUE), primarily
+%     to enable ramping feature
+%   - required hack for very small float16 values (try statement skips such
+%     values silently)
 
 global smdata;
 
@@ -63,16 +68,30 @@ switch ico(2)
                 else
                     dec16 = round(2^16 - abs(voltage)/10 * 2^15); %Decimal equivalent of 16 bit data
                 end
-                bin16 = de2bi(dec16,16,2,'left-msb'); %16 bit binary
-                d1=bi2de(fliplr(bin16(1:8))); %first 8 bits
-                d2=bi2de(fliplr(bin16(9:16))); %second 8 bits
-                % disp([255,254,253,n1,d1*m1,d2*m1,n2,d1*m2,d2*m2]); %uncomment to check what’s being sent to the Arduino
-                pause(.005);
-                fwrite(smdata.inst(ico(1)).data.inst,[255,254,253,n1,d1*m1,d2*m1,n2,d1*m2,d2*m2]);
-                while smdata.inst(ico(1)).data.inst.BytesAvailable
-                    fscanf(smdata.inst(ico(1)).data.inst,'%e');
+                
+                try % hack to avoid error from especially small numbers
+                    bin16 = de2bi(dec16,16,2,'left-msb'); %16 bit binary
+                    d1=bi2de(fliplr(bin16(1:8))); %first 8 bits
+                    d2=bi2de(fliplr(bin16(9:16))); %second 8 bits
+                    % disp([255,254,253,n1,d1*m1,d2*m1,n2,d1*m2,d2*m2]); %uncomment to check what’s being sent to the Arduino
+                    pause(.005);
+                    fwrite(smdata.inst(ico(1)).data.inst,[255,254,253,n1,d1*m1,d2*m1,n2,d1*m2,d2*m2]);
+                    while smdata.inst(ico(1)).data.inst.BytesAvailable
+                        fscanf(smdata.inst(ico(1)).data.inst,'%e');
+                    end
+
+                    % store last SET value in smdata.inst.data to enable GET
+                    smdata.inst(ico(1)).data.last_set_value{ico(2)} = voltage;
+%                 catch
+%                     PAUSE HERE IN DEBUG MODE TO TEST SMALL 16-bit FLOATS;
                 end
-            otherwise %unable to read the voltage
+            case 0 % NOT A TRUE READ ACTION; SIMPLY YIELDS last_set_value
+                if ~isempty(smdata.inst(ico(1)).data.last_set_value{ico(2)})
+                    val = smdata.inst(ico(1)).data.last_set_value{ico(2)};
+                else
+                    error(sprintf('No prior SET value given for %s channel %d', smdata.inst(ico(1)).name, ico(2)));
+                end
+            otherwise
                 error('Operation not supported');
         end
     otherwise
